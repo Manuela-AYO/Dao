ef6e716534a9fd33d598468742fa1b93cb4f3f38
Manuela-AYO <manuelaayo7@gmail.com>
2024-06-02 19:37:48 +0200

    Tuto local blockchain

---
+++ localBlockChain/scripts/init.js
@@ -0,0 +1,15 @@
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+async function init() {
+  const address = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
+
+  await helpers.setBalance(address, 15000 * 1e18);
+  console.log("Done");
+}
+
+init()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
